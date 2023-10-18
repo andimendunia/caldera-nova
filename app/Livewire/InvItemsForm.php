3da40619e929ccfe43ac862dc666c758a04aab55
Andi Permana <andi.thea96@gmail.com>
2023-10-19 01:05:35 +0700

    InvItemsForm update

--- app/Livewire/InvItemsForm.php
+++ app/Livewire/InvItemsForm.php
@@ -36,8 +36,8 @@ class InvItemsForm extends Component
     // public $qty_main;
     // public $qty_used;
     // public $qty_rep;
-    public $qty_main_min;
-    public $qty_main_max;
+    public $qty_main_min = 0;
+    public $qty_main_max = 0;
     public $denom = 1;
     public $up = 0;
     public $photo;
@@ -50,17 +50,14 @@ class InvItemsForm extends Component
             'desc'      => ['required', 'min:1', 'max:256'],
             'code'      => ['nullable', 'size:11'],
             'curr_id'   => ['nullable', 'integer'],
-            'price'     => ['nullable', 'numeric', 'min:0', 'max:999000000'],
-            'price_sec' => ['nullable', 'numeric', 'min:0', 'max:999000000'],
-            // 'qty_main'  => ['nullable', 'integer', 'min:0', 'max:99999'],
-            // 'qty_used'  => ['nullable', 'integer', 'min:0', 'max:99999'],
-            // 'qty_rep'   => ['nullable', 'integer', 'min:0', 'max:99999'],
+            'price'     => ['required', 'numeric', 'min:0', 'max:999000000'],
+            'price_sec' => ['required', 'numeric', 'min:0', 'max:999000000'],
             'uom'       => ['required', 'min:1', 'max:5'],
-            'denom'     => ['nullable', 'integer', 'min:1', 'max:1000'],
+            'denom'     => ['required', 'integer', 'min:1', 'max:1000'],
             'loc'       => ['nullable', 'alpha_dash', 'max:20'],
             'tags.*'    => ['nullable', 'alpha_dash', 'max:20'],
-            'qty_main_min'  => ['nullable', 'integer', 'min:0', 'max:99999'],
-            'qty_main_max'  => ['nullable', 'integer', 'min:0', 'max:99999'],
+            'qty_main_min'  => ['required', 'integer', 'min:0', 'max:99999'],
+            'qty_main_max'  => ['required', 'integer', 'min:0', 'max:99999'],
         ];
     }
 
@@ -107,14 +104,16 @@ class InvItemsForm extends Component
 
     public function updatedPrice()
     {
-        $rate = $this->curr_sec->rate ?? 0;
-        $this->price_sec = $rate ? round(((double)$this->price * (double)$rate), 2) : 0;
+        $rate   = (double)($this->curr_sec->rate ?? 0);
+        $price  = (double)$this->price;
+        $this->price_sec = round(($rate > 0 ? ($price * $rate) : 0), 2);
     }
 
     public function updatedPriceSec()
     {
-        $rate = $this->curr_sec->rate ?? 0;
-        $this->price = $rate ? round(((double)$this->price_sec / (double)$rate), 2) : 0;
+        $rate       = (double)$this->curr_sec->rate ?? 0;
+        $price_sec  = (double)$this->price_sec;
+        $this->price = round(($rate > 0 ? ($price_sec / $rate) : 0), 2);
         $this->calcUp();
     }
 
@@ -136,11 +135,7 @@ class InvItemsForm extends Component
         $denom = (int)$this->denom;
         $uom = $this->uom;
 
-        if ($price > 0 && $denom > 1 && $uom) {
-            $this->up = round(($price / $denom), 2);
-        } else {
-            $this->up = 0;
-        }
+        $this->up = round((($price && $denom > 1 && $uom) ? ($price / $denom) : 0), 2);
     }
 
     public function updated($property)
@@ -155,10 +150,23 @@ class InvItemsForm extends Component
 
         // validate if photo url exist
 
-        // (double)price, and (double)price_sec
+        $curr       = InvCurr::find((int)$this->curr_id);
+        $rate       = (double)($curr ? $curr->rate : 1);
+        $price      = (double)$this->price;
+        $price_sec  = (double)$this->price_sec;
+        $denom      = (int)$this->denom;
 
-        // if curr_id existdb && price_sec notzero, calculate (double)rate and (double)price_main
-        // else curr_id = null, price_sec = 0
+        if($curr && $price_sec && $rate > 0) {
+            $this->price     = $price_sec / $rate;
+        } else {
+            $this->curr_id   = '';
+            $this->price     = $price;
+        }
+
+        $this->price_sec = $price_sec; 
+        $this->qty_main_min   = (int)$this->qty_main_min;
+        $this->qty_main_max   = (int)$this->qty_main_max;
+        $this->denom = $denom > 0 ? $denom : 1;
 
         $props = ['name', 'desc', 'code', 'uom', 'loc'];
         foreach($props as $prop) {
