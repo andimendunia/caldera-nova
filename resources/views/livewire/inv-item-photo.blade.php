3ef80a12d5463452b9329acbfb2989b6990e84a4
Andi Permana <andi.thea96@gmail.com>
2023-10-19 13:24:08 +0700

    Minor layout changes

--- resources/views/livewire/inv-item-photo.blade.php
+++ resources/views/livewire/inv-item-photo.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <div
+        class="flex w-full rounded-none sm:w-48 h-48 md:w-72 md:h-72 lg:w-80 lg:h-80 bg-neutral-200 dark:bg-neutral-700 sm:rounded">
+        <div class="m-auto">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                class="block h-32 w-auto fill-current text-neutral-800 dark:text-neutral-200 opacity-25"
+                viewBox="0 0 38.777 39.793">
+                <path
+                    d="M19.396.011a1.058 1.058 0 0 0-.297.087L6.506 5.885a1.058 1.058 0 0 0 .885 1.924l12.14-5.581 15.25 7.328-15.242 6.895L1.49 8.42A1.058 1.058 0 0 0 0 9.386v20.717a1.058 1.058 0 0 0 .609.957l18.381 8.633a1.058 1.058 0 0 0 .897 0l18.279-8.529a1.058 1.058 0 0 0 .611-.959V9.793a1.058 1.058 0 0 0-.599-.953L20 .105a1.058 1.058 0 0 0-.604-.095zM2.117 11.016l16.994 7.562a1.058 1.058 0 0 0 .867-.002l16.682-7.547v18.502L20.6 37.026V22.893a1.059 1.059 0 1 0-2.117 0v14.224L2.117 29.432z" />
+            </svg>
+        </div>
+    </div>
+    <div class="p-4 text-sm text-neutral-600 dark:text-neutral-400">
+        <div class="mb-4">
+            <x-link href="#"><i
+                    class="fa fa-fw fa-upload mr-3"></i>{{ __('Unggah foto') }}</x-link><br />
+        </div>
+        <div>
+            <x-link href="#"><i
+                    class="fa fa-fw fa-file-import mr-3"></i>{{ __('Tarik dari ttconsumable') }}</x-link>
+        </div>
+    </div>
 </div>
