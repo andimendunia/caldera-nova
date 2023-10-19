7d19e085750339517505f5def82c825eb95ef9f1
Andi Permana <andi.thea96@gmail.com>
2023-10-20 01:19:50 +0700

    InvItemPhoto upload feature

--- app/Livewire/InvItemPhoto.php
+++ app/Livewire/InvItemPhoto.php
@@ -3,9 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\WithFileUploads;
 
 class InvItemPhoto extends Component
 {
+    use WithFileUploads;
+    
+    #[Rule('image|max:1024')] // 1MB Max
+    public $photo;
+
     public function render()
     {
         return view('livewire.inv-item-photo');
