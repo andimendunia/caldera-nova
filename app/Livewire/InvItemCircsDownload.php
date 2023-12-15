ab579044714d3d103e61302871dca4a02dbda49e
Andi Permana <andi.thea96@gmail.com>
2023-12-15 16:31:12 +0700

    Updates

--- app/Livewire/InvItemCircsDownload.php
+++ app/Livewire/InvItemCircsDownload.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class InvItemCircsDownload extends Component
 {
+    public function placeholder()
+    {
+        return view('livewire.modal-placeholder');
+    }
+    
     public function render()
     {
         return view('livewire.inv-item-circs-download');
     }
+
+    
 }
--- resources/views/livewire/inv-item-circs-download.blade.php
+++ resources/views/livewire/inv-item-circs-download.blade.php
@@ -1,3 +1,21 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="p-6">
+    <div class="flex justify-between items-center text-lg mb-6 font-medium text-neutral-900 dark:text-neutral-100">
+        <h2>
+            {{ __('Unduh sirkulasi barang') }}
+        </h2>
+        <x-text-button type="button" x-on:click="$dispatch('close')"><i class="fa fa-times"></i></x-text-button>
+    </div>
+    <form wire:submit.prevent="download">
+        <div class="mb-6">
+            <x-input-label for="inv-date-start" :value="__('Dari')" />
+            <x-text-input wire:model.live="start_at" id="inv-date-start" type="date"></x-text-input>
+        </div>
+        <div class="mb-6">
+            <x-input-label for="inv-date-end" :value="__('Sampai')" />
+            <x-text-input wire:model.live="end_at" id="inv-date-end" type="date"></x-text-input>
+        </div>
+        <x-primary-button type="submit"><i class="fa fa-download mr-2"></i>{{ __('Unduh') }}</x-primary-button>
+    </form>
+    <x-spinner-bg wire:loading.class.remove="hidden" wire:target="download"></x-spinner-bg>
+    <x-spinner wire:loading.class.remove="hidden"  wire:target="download" class="hidden"></x-spinner>
 </div>
