1b9bf2a34516468b80708dd147c42f70c3774756
Andi Permana <andi.thea96@gmail.com>
2024-01-22 15:48:22 +0700

    Update

--- resources/views/kpi/overview.blade.php
+++ resources/views/kpi/overview.blade.php
@@ -1,4 +1,252 @@
-<div id="content" class="py-8 max-w-5xl mx-auto sm:px-6 lg:px-8 text-neutral-800 dark:text-neutral-200">
-    <div class="text-center">Overview here</div>
+<div id="content" class="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8 text-neutral-800 dark:text-neutral-200">
+    <div class="flex mb-5">
+
+        <div class="flex">
+            <x-select>
+                <option value="2024">2024</option>
+            </x-select>
+        </div>
+    </div>
+    <div class="bg-white dark:bg-neutral-800 shadow sm:rounded-lg overflow-x-auto">
+        <table class="table">
+            <tr class="uppercase text-xs">
+                <th>
+                    {{ __('No') }}
+                </th>
+                <th>
+                    {{ __('Nama') }}
+                </th>
+                <th>
+                    {{ __('JAN') }}
+                </th>
+                <th>
+                    {{ __('FEB') }}
+                </th>
+                <th>
+                    {{ __('MAR') }}
+                </th>
+                <th>
+                    {{ __('APR') }}
+                </th>
+                <th>
+                    {{ __('MEI') }}
+                </th>
+                <th>
+                    {{ __('JUN') }}
+                </th>
+                <th>
+                    {{ __('JUL') }}
+                </th>
+                <th>
+                    {{ __('AGT') }}
+                </th>
+                <th>
+                    {{ __('SEP') }}
+                </th>
+                <th>
+                    {{ __('OKT') }}
+                </th>
+                <th>
+                    {{ __('NOV') }}
+                </th>
+                <th>
+                    {{ __('DES') }}
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    1
+                </td>
+                <td>
+                    Nama butir/deskripsi KPI
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    2
+                </td>
+                <td>
+                    Nama butir/deskripsi KPI butir kedua
+                </td>
+                <td>
+                    <div>
+                        <div>1000</div>
+                        <div class="text-xs"><i class="fa fa-paperclip mr-1"></i>2</div>
+                    </div>
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    3
+                </td>
+                <td>
+                    Nama butir/deskripsi KPI butir ketiga
+                </td>
+                <td>
+                    <div>
+                        <div>1000</div>
+                        <div class="text-xs"><i class="fa fa-paperclip mr-1"></i>2</div>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <div>1000</div>
+                        <div class="text-xs"><i class="fa fa-paperclip mr-1"></i>2</div>
+                    </div>
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    4
+                </td>
+                <td>
+                    Nama butir/deskripsi KPI butir keempat
+                </td>
+                <td>
+                    <div>
+                        <div>1000</div>
+                        <div class="text-xs"><i class="fa fa-paperclip mr-1"></i>2</div>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <div>1000</div>
+                        <div class="text-xs"><i class="fa fa-paperclip mr-1"></i>2</div>
+                    </div>
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+                <td>
+                    1000
+                </td>
+            </tr>
+            
+
+        </table>
+    </div>
+
 </div>
-    
