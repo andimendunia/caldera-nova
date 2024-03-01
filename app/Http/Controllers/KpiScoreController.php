f2aff144f877253b880ab1e770dad126d96326f0
Andi Permana <andi.thea96@gmail.com>
2024-03-01 14:31:07 +0700

    Updates

--- app/Http/Controllers/KpiScoreController.php
+++ app/Http/Controllers/KpiScoreController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KpiItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class KpiScoreController extends Controller
 {
-    //
+    public function show()
+    {
+        Gate::authorize('viewAny', KpiItem::class);
+
+        $prev = route('kpi', [ 'nav' => 'submission' ]);
+        $navs = true;
+        $header = '';
+        $title = '';
+
+        return view('kpi.scores.show', compact('title', 'prev', 'header', 'navs'));
+    }
 }
---
+++ resources/views/kpi/scores/show.blade.php
@@ -0,0 +1,3 @@
+<x-layout-kpi title="{{ $title }}" header="{{ $header }}" prev="{!! $prev !!}" nav="submission" navs="{{ $navs }}">
+Hello
+</x-layout-kpi>
